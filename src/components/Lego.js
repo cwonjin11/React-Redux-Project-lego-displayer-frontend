a32b51c7aaa5e55c26359dcdcdb0e602d0fc5375
Wonjin Cho <cwonjin11@gmail.com>
2021-07-23 13:38:42 -0400

    card-back image added

--- src/components/Lego.js
+++ src/components/Lego.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { removeLego } from '../redux/actions/index'
 import { editLego } from '../redux/actions/index'
 
+
 const Lego = ({ lego, removeLego, editLego }) => {
     return (
         <div className="lego-card">
@@ -13,17 +14,17 @@ const Lego = ({ lego, removeLego, editLego }) => {
                         <img className="lego-image" src={lego.image} alt={lego.name} />
                     </div>
                     <div className="flip-card-back">
-                        {/* <img className="lego-image-small" src={lego.image} alt={lego.name} /> */}
-                        <p className="card-back-text">Lego Name : {lego.name} </p>
+                        <img className="lego-image-small" src={lego.image} alt={lego.name} />
+                        <p className="lego-name">{lego.name} </p>
                         <p>Item Number: {lego.item_number}</p>
                         <p>Pieces: {lego.pieces}</p>
                         <p>Prices: {lego.prices}</p>
                     </div>
                 </div>
             </div>
-            <div className="lego-init-info">
+            <div>
                 <p className="lego-name">{lego.name}</p>
-                <p className="lego-item_number">{lego.item_number}</p>
+                <p className="lego-item_number"> Item Number : {lego.item_number}</p>
                 <NavLink to={`/legos/${lego.id}/edit`}><button className="btn" onClick={()=>editLego(lego)}>Edit</button></NavLink>
                 <button className="delete-btn" onClick={ () => removeLego(lego.id) }>Delete</button>
             </div>
