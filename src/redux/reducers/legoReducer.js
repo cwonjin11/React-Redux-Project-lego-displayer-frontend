36521f997c414de467fdaaaf78e30a2274f3ad41
Wonjin Cho <cwonjin11@gmail.com>
2021-07-23 13:37:44 -0400

    edit case code refactored with !== notation

--- src/redux/reducers/legoReducer.js
+++ src/redux/reducers/legoReducer.js
@@ -3,16 +3,22 @@ export default ( state = [], action ) => {
     switch(action.type) {
         case "FETCH_LEGOS":
             return [...action.payload]  // similar to params in rails
-        case "ADD_LEGO":
-            return [...state, action.payload]
+        case "ADD_LEGO":  
+            return [...state, action.payload]  //5. The reducer then returns a NEW version of our state which triggers any component that receives our redux state as props to re-render
         case "REMOVE_LEGO":
             let updatedLegos = state.filter(lego => lego.id !== action.payload) //lego id is not equal to action.payload
             return [...updatedLegos]  //spreading out updatedLegos 
         case "EDIT_LEGO":
-            let legosAll = [...state]
-            legosAll.findIndex(lego => lego.id === action.payload.id )
-            return  [...legosAll, action.payload]
+
+            let legoAll = state.filter(lego => lego.id !== action.payload)
+            return [...legoAll, action.payload]
+            // or
+            // let legosAll = [...state]
+            // legosAll.findIndex(lego => lego.id === action.payload.id )
+            // return  [...legosAll, action.payload]
         default:
             return state
     }
 }
+
+//my reducer is handling my redux store
